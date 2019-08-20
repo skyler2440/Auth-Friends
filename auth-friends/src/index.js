c8a89e13460f1472db421b71e5c8916aeda5f8bd
skyler dowdy <skylerwebdev@gmail.com>
2019-08-20 15:19:06 -0400

    project setup and login gets token

---
+++ auth-friends/src/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import {reducer} from "./reducers";
+import App from "./App";
+
+
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
