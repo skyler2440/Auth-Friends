bef08d34e7c827c0c2ec4f434817a749728780d8
skyler dowdy <skylerwebdev@gmail.com>
2019-08-20 15:19:06 -0400

    Revert "Revert "project setup and login gets token""
    This reverts commit 01b9baf600aef2b27fea24350f734c80b7905f0e.

---
+++ auth-friends/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+    <LoginForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
